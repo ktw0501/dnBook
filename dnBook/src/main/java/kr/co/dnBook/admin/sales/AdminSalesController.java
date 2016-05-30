0cf6819f77234689b5e635fc8b4b898649ce5af0
choi <chaose37@naver.com>
2016-05-30 20:08:43 +0900

    수정

--- dnBook/src/main/java/kr/co/dnBook/admin/sales/AdminSalesController.java
+++ dnBook/src/main/java/kr/co/dnBook/admin/sales/AdminSalesController.java
@@ -1,8 +1,20 @@
 package kr.co.dnBook.admin.sales;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/sales")
 public class AdminSalesController {
+	@RequestMapping("/daily.do")
+	public void DailySales() throws Exception{}
+	@RequestMapping("/weekly.do")
+	public void WeeklySales() throws Exception{}
+	@RequestMapping("/monthly.do")
+	public void MonthlySales() throws Exception{}
+	@RequestMapping("/hourly.do")
+	public void HourlySales() throws Exception{}
+
+	
 
 }
